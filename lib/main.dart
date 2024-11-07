0deb3b7b72af233f7f9362c656df5db0860b0710
VanLinh1708 <linh.van.170898@gmail.com>
2024-11-07 14:46:37 +0700

    add code test

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,83 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/fortune_wheel/fortune_wheel.dart';
+import 'js_interop.dart'; // Nhập file interop Dart
 
 void main() {
-  HttpOverrides.global = MyHttpOverrides();
-
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyFortuneWheel(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHttpOverrides extends HttpOverrides {
+class HomeScreen extends StatefulWidget {
   @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _controller = TextEditingController();
+  String _storedValue = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadStoredValue();
+  }
+
+  // Gọi hàm JavaScript để lưu dữ liệu vào localStorage
+  _saveToLocalStorage() {
+    saveToLocalStorage('myVariable', _controller.text); // Gọi JavaScript
+    _loadStoredValue(); // Tải lại giá trị từ localStorage
+  }
+
+  // Gọi hàm JavaScript để lấy dữ liệu từ localStorage
+  _loadStoredValue() {
+    setState(() {
+      _storedValue = getFromLocalStorage('myVariable') ??
+          "Chưa có dữ liệu"; // Gọi JavaScript
+    });
+  }
+
+  // Gọi hàm JavaScript để xóa dữ liệu khỏi localStorage
+  _clearStoredValue() {
+    removeFromLocalStorage('myVariable'); // Gọi JavaScript
+    _loadStoredValue(); // Tải lại giá trị từ localStorage
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Web LocalStorage Example')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Nhập giá trị'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveToLocalStorage,
+              child: Text('Lưu Dữ Liệu'),
+            ),
+            SizedBox(height: 20),
+            Text('Giá trị đã lưu: $_storedValue'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _clearStoredValue,
+              child: Text('Xóa Dữ Liệu'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
